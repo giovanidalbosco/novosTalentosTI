2d7337b1903fb8bb26a5e8a85424847b22142dda
giovani <giovani.dalbosco@gmail.com>
2022-08-15 08:52:45 -0300

    atualização do repositório

--- modulo1/giovanidalbosco/src/teste.java
+++ modulo1/giovanidalbosco/src/teste.java
@@ -1,7 +1,30 @@
-public class teste {
-    public static void main(String[] args) {
-        char letra = 'a';
+import java.util.*; 
+import java.io.*;
 
-        System.out.println(String.valueOf(letra));
+
+class Main {
+
+    public static String ArrayChallenge(String[] strArr) {
+        return strArr[0];
     }
+
+    public static void main (String[] args) {  
+        // keep this function call here     
+        Scanner s = new Scanner(System.in);
+        String a = s.nextLine();
+        System.out.print(a); 
+
+        String[] ave = new String[] {"baseball", "a,all,b,ball,bas,base,cat,code,d,e,quit,z"};
+        
+        String[] lista = ave[1].split(",");
+
+        System.out.println(lista[0]);
+        for (int i = 0; i < lista.length; i++ ) {
+            System.out.println(lista[i]);
+        }
+
+    }
+
 }
+
+
