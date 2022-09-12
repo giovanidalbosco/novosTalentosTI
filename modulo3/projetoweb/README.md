63d3fef58560a69a9b472ee44b2668200eb59936
giovani <giovani.dalbosco@gmail.com>
2022-09-12 17:15:38 -0300

    avançando com o projetoweb

--- modulo3/projetoweb/README.md
+++ modulo3/projetoweb/README.md
@@ -1,5 +1,11 @@
 
 # Os pacotes do Express.js não são commitados no repositório do git
+## npm i express-handlebars --save
+## npm i bootstrap --save
+## npm i body-parser --save
+## npm i jquery --save
+
 ## para instalar os pacotes digitar "npm i"
+## para rodar o projeto digitar "node index.js"
 
 12factor.net/pt_br/ --> leitura obrigatória
--- modulo3/projetoweb/index.js
+++ modulo3/projetoweb/index.js
@@ -1,15 +1,42 @@
 const express = require('express'); // é o jeito de importar do node.
 const { engine } = require('express-handlebars');
 const app = express();
-
+const path = require('path');
 
 app.set('view engine', 'handlebars'); //seta o motor de renderização para o handlebars
 app.engine('handlebars', engine()); //mostra para o node qual o motor será usado
 
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')))
+app.use('/public', express.static(path.join(__dirname, 'public')))
+
+const fakeData = [ //lista de objetos
+    {
+        id: 1,
+        nome: 'Zezinho',
+        endereco: 'Rua lalala',
+        sexo: 'Masculino',
+        telefone: '5555-1234',
+    },
+    {
+        id: 2,
+        nome: 'Mariazinha',
+        endereco: 'Rua lululu',
+        sexo: 'Feminino',
+        telefone: '7777-4321',
+    }
+];
+
+
 app.get("/", function(req, res) {
     //res.send(`<h1>Eu não acredito! ${req.query['nome']} ${req.query['sobrenome']}</h1>`); // para passar o parâmetro nome é necessário fazer conforme -> http://localhost:3000/?nome=Zé
     res.render('index');
-})
+});
+
+app.get("/clientes", function(req, res) {
+    res.render('cliente/cliente', {fakeData: fakeData});
+});
 
 app.listen(3000, () => {
     console.log("Servidor online");
