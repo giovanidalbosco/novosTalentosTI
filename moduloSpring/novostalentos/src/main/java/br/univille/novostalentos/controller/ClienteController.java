b422e710f2bc6a1d56e92e63626546ecf603c3d8
GIOVANI VALLE DALBOSCO <giovani.dalbosco@univille.br>
2022-09-29 17:18:40 -0300

    atualizado projeto Spring Framework

--- moduloSpring/novostalentos/src/main/java/br/univille/novostalentos/controller/ClienteController.java
+++ moduloSpring/novostalentos/src/main/java/br/univille/novostalentos/controller/ClienteController.java
@@ -2,43 +2,51 @@ package br.univille.novostalentos.controller;
 
 import java.util.ArrayList;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.univille.novostalentos.entity.Cliente;
+import br.univille.novostalentos.repository.ClienteRepository;
 
 @Controller
 @RequestMapping("/clientes")
 public class ClienteController {
     
+    //esse código será removido - controlador não deve chamar repositorio
+    @Autowired
+    private ClienteRepository repositorio;
+
     @GetMapping
     public ModelAndView index() {
-        ArrayList<Cliente> listaClientes = new ArrayList<>();
+        // ArrayList<Cliente> listaClientes = new ArrayList<>();
 
-        Cliente cliente1 = new Cliente();
-        cliente1.setId(1);
-        cliente1.setNome("Luizinho");
-        cliente1.setEndereco("Rua lelele");
-        cliente1.setSexo("Masculino");
+        // Cliente cliente1 = new Cliente();
+        // // cliente1.setId(1);
+        // cliente1.setNome("Luizinho");
+        // cliente1.setEndereco("Rua lelele");
+        // cliente1.setSexo("Masculino");
 
-        Cliente cliente2 = new Cliente();
-        cliente2.setId(2);
-        cliente2.setNome("Mariazinha");
-        cliente2.setEndereco("Rua lululu");
-        cliente2.setSexo("Feminino");
+        // Cliente cliente2 = new Cliente();
+        // // cliente2.setId(2);
+        // cliente2.setNome("Mariazinha");
+        // cliente2.setEndereco("Rua lululu");
+        // cliente2.setSexo("Feminino");
 
-        Cliente cliente3 = new Cliente();
-        cliente3.setId(3);
-        cliente3.setNome("Zezinho");
-        cliente3.setEndereco("Rua lalala");
-        cliente3.setSexo("Masculino");
+        // Cliente cliente3 = new Cliente();
+        // // cliente3.setId(3);
+        // cliente3.setNome("Zezinho");
+        // cliente3.setEndereco("Rua lalala");
+        // cliente3.setSexo("Masculino");
 
-        listaClientes.add(cliente1);
-        listaClientes.add(cliente2);
-        listaClientes.add(cliente3);
+        // listaClientes.add(cliente1);
+        // listaClientes.add(cliente2);
+        // listaClientes.add(cliente3);
 
+
+        var listaClientes = repositorio.findAll();
         return new ModelAndView("cliente/index", "listaClientes", listaClientes);
     }
 }
--- moduloSpring/novostalentos/src/main/java/br/univille/novostalentos/entity/Cliente.java
+++ moduloSpring/novostalentos/src/main/java/br/univille/novostalentos/entity/Cliente.java
@@ -1,10 +1,40 @@
 package br.univille.novostalentos.entity;
 
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
 public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @Column(length=100, nullable=false)
     private String nome;
     private String endereco;
     private String sexo;
+    @Temporal(value= TemporalType.DATE)
+    private Date dataNascimento;
+    private static int teste = 0;
+
+    public Cliente(){
+        teste++;
+        this.id = teste;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
 
     public long getId() {
         return id;
