3512d35a5a9c50f30d3c72251f9768dd7b91e55e
giovani <giovani.dalbosco@gmail.com>
2022-10-06 23:47:27 -0300

    avançando projeto Spring

--- moduloSpring/novostalentos/src/main/resources/static/script.js
+++ moduloSpring/novostalentos/src/main/resources/static/script.js
@@ -1,18 +1,18 @@
 (function(){
     $("#tabclientes").on("click", ".js-delete", function(){
         let botaoClicado = $(this);
-        $("btnsim").attr("data-id", botaoClicado.attr("data-id")); 
-        $("modalcliente").modal("show");
+        $("#btnsim").attr("data-id",botaoClicado.attr("data-id"));
+        $("#modalcliente").modal("show");
     });
 
-    $("#btnsim").on("click", function(){
+    $("#btnsim").on("click",function(){
         let botaoSim = $(this);
-        let id = botaoSim.attr("data-id")
+        let id = botaoSim.attr("data-id");
         $.ajax({
             url: "/clientes/delete/" + id,
             method: "GET",
             success: function() {
-                windows.location.href="/clientes";
+                window.location.href="/clientes";
             }
         });
     });
