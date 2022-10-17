e348c167198ab251e83fa1d7fabf3f922e8cade0
GIOVANI VALLE DALBOSCO <giovani.dalbosco@univille.br>
2022-10-17 17:12:48 -0300

    avançando projeto Spring

--- moduloSpring/novostalentos/src/main/java/br/univille/novostalentos/entity/Cliente.java
+++ moduloSpring/novostalentos/src/main/java/br/univille/novostalentos/entity/Cliente.java
@@ -11,6 +11,12 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.FutureOrPresent;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.util.pattern.PatternParseException.PatternMessage;
 
 @Entity
 public class Cliente {
@@ -18,10 +24,16 @@ public class Cliente {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     @Column(length=100, nullable=false)
+    @NotBlank(message = "Campo nome não poder ser em branco")
     private String nome;
     private String endereco;
+    @Pattern(regexp = "Masculino|Feminino",
+            flags = Pattern.Flag.CANON_EQ,
+            message = "Valor inválido, utiliza Masculino ou Feminino")
     private String sexo;
     @Temporal(value= TemporalType.DATE)
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @FutureOrPresent
     private Date dataNascimento;
     // @ManyToOne
     // @JoinColumn(name = "cidade_id", nullable=false)
